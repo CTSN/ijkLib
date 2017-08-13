d17a45912a6e4e6da1de93a118f4aabb2478d8c8
CTSN <519405585@qq.com>
2017-08-13 18:20:53 +0800

    提交代码

--- ijklibirry/src/main/java/com/roy/ijklibirry/TestActivity.java
+++
@@ -1,8 +0,0 @@
-package com.roy.ijklibirry;
-
-/**
- * Created by roy on 2017/8/13.
- */
-
-public class TestActivity {
-}
--- sample/src/main/java/com/roy/ijklib/MainActivity.java
+++ sample/src/main/java/com/roy/ijklib/MainActivity.java
@@ -18,22 +18,17 @@ import com.roy.ijklibirry.media.IjkVideoView;
 
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
 
     private String mVideoPath;
-    private Uri    mVideoUri;
 
     private AndroidMediaController mMediaController;
     private IjkVideoView mVideoView;
-    private TextView mToastTextView;
     private TableLayout mHudView;
-    private DrawerLayout mDrawerLayout;
-    private ViewGroup mRightDrawer;
 
-//    private Settings mSettings;
     private boolean mBackPressed;
 
 
@@ -42,27 +37,21 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mSettings = new Settings(this);
 
         // handle arguments
         mVideoPath = "http://fs.mv.android.kugou.com/201708131754/c113b5890cd8b4ff64e6d69bfa2d63f9/G104/M07/05/13/SJQEAFmEQ-aAaMvdAM3BE6bN56I253.mp4";
 
 
-
 //        // init UI
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         mMediaController = new AndroidMediaController(this, false);
         mMediaController.setSupportActionBar(actionBar);
 
-        mToastTextView = (TextView) findViewById(R.id.toast_text_view);
         mHudView = (TableLayout) findViewById(R.id.hud_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mRightDrawer = (ViewGroup) findViewById(R.id.right_drawer);
 
-        mDrawerLayout.setScrimColor(Color.TRANSPARENT);
 
         // init player
         IjkMediaPlayer.loadLibrariesOnce(null);
@@ -74,8 +63,6 @@ public class MainActivity extends AppCompatActivity{
         // prefer mVideoPath
         if (mVideoPath != null)
             mVideoView.setVideoPath(mVideoPath);
-        else if (mVideoUri != null)
-            mVideoView.setVideoURI(mVideoUri);
         else {
             Log.e(TAG, "Null Data Source\n");
             finish();
@@ -105,79 +92,4 @@ public class MainActivity extends AppCompatActivity{
         IjkMediaPlayer.native_profileEnd();
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_player, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.action_toggle_ratio) {
-//            int aspectRatio = mVideoView.toggleAspectRatio();
-//            String aspectRatioText = MeasureHelper.getAspectRatioText(this, aspectRatio);
-//            mToastTextView.setText(aspectRatioText);
-//            mMediaController.showOnce(mToastTextView);
-//            return true;
-//        } else if (id == R.id.action_toggle_player) {
-//            int player = mVideoView.togglePlayer();
-//            String playerText = IjkVideoView.getPlayerText(this, player);
-//            mToastTextView.setText(playerText);
-//            mMediaController.showOnce(mToastTextView);
-//            return true;
-//        } else if (id == R.id.action_toggle_render) {
-//            int render = mVideoView.toggleRender();
-//            String renderText = IjkVideoView.getRenderText(this, render);
-//            mToastTextView.setText(renderText);
-//            mMediaController.showOnce(mToastTextView);
-//            return true;
-//        } else if (id == R.id.action_show_info) {
-//            mVideoView.showMediaInfo();
-//        } else if (id == R.id.action_show_tracks) {
-//            if (mDrawerLayout.isDrawerOpen(mRightDrawer)) {
-//                Fragment f = getSupportFragmentManager().findFragmentById(R.id.right_drawer);
-//                if (f != null) {
-//                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//                    transaction.remove(f);
-//                    transaction.commit();
-//                }
-//                mDrawerLayout.closeDrawer(mRightDrawer);
-//            } else {
-//                Fragment f = TracksFragment.newInstance();
-//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//                transaction.replace(R.id.right_drawer, f);
-//                transaction.commit();
-//                mDrawerLayout.openDrawer(mRightDrawer);
-//            }
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    @Override
-//    public ITrackInfo[] getTrackInfo() {
-//        if (mVideoView == null)
-//            return null;
-//
-//        return mVideoView.getTrackInfo();
-//    }
-//
-//    @Override
-//    public void selectTrack(int stream) {
-//        mVideoView.selectTrack(stream);
-//    }
-//
-//    @Override
-//    public void deselectTrack(int stream) {
-//        mVideoView.deselectTrack(stream);
-//    }
-//
-//    @Override
-//    public int getSelectedTrack(int trackType) {
-//        if (mVideoView == null)
-//            return -1;
-//
-//        return mVideoView.getSelectedTrack(trackType);
-//    }
 }
